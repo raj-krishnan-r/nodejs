c87122f9484901061b3c3ab8e69a50a87dd4d6d3
Rajkrishnan <rkrishnan1993@live.com>
2014-12-28 03:51:20 +0530

    Kyu ye dolae , janoo na

--- resemble.js
+++ resemble.js
@@ -1,10 +1,33 @@
+var rcount=0;
 var http = require('http');
-var http = require('fs');
+var fs = require('fs');
+var net = require('net');
 
-var content=fs.open('first.html',r);
-var server = http.createServer(function(request, response)
+var content = '';
+
+console.log("Server Initialized...");
+
+var server = http.createServer(function(request, response,socket)
 {
+	//Code to fetch file contents
+	fs.readFile('./first.html',function(err,data)
+{
+	content=data;
+	console.log(content);
 	response.write(content);
+	rcount++;
+	console.log('Request : '+rcount);
+	response.statuscode=200;
 	response.end();
+
+	if(err!=null)
+	{
+			console.log(err);
+
+	}
+});
+
+//Code segments ends here
+
 });
 server.listen(3000);
