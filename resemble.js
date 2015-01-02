e850a34a38d7278f2242227b25bc4840d00b1101
Rajkrishnan <rkrishnan1993@live.com>
2015-01-02 15:36:50 +0530

    minor changes

--- resemble.js
+++ resemble.js
@@ -2,22 +2,20 @@ var rcount=0;
 var http = require('http');
 var fs = require('fs');
 var net = require('net');
-
 var content = '';
-
 console.log("Server Initialized...");
-
 var server = http.createServer(function(request, response,socket)
 {
 	//Code to fetch file contents
 	fs.readFile('./first.html',function(err,data)
 {
+	var time = new Date;
 	content=data;
-	console.log(content);
+	content='<!--Served from Greed-2 [Node.js server] on '+time+'  -->\n'+content;
 	response.write(content);
 	rcount++;
 	console.log('Request : '+rcount);
-	response.statuscode=200;
+	response.statusCode=404;
 	response.end();
 
 	if(err!=null)
